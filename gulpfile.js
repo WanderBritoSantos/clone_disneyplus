fbffe3e61fab772e35ad011272f12a23d2fa3c2d
Wanderson Brito Santos <wbsmsn@hotmail.com>
2025-07-18 15:21:42 -0300

    configurando o gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin =require('gulp-imagemin');
 const newer = require('gulp-newer');
+const uglify = require('gulp-uglify');
+
+function script(){
+   return gulp.src('./src/script/*.js')   //aponta para o arquivo que vai ser compilado 
+    .pipe(uglify())// comprime o arquivo deixando ele mais compacto
+    .pipe(gulp.dest('./dist/js')); //destino do arquivo final compilado 
+}
 
 function styles(){
    return gulp.src('./src/styles/*.scss')   //aponta para o arquivo que vai ser compilado 
@@ -25,7 +32,10 @@ function images() {
 
 
 
-exports.default = gulp.parallel(styles,images);
+exports.default = gulp.parallel(styles,images,script);
+
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss',gulp.parallel(styles))
+    gulp.watch('./src/script/*.scss',gulp.parallel(script))
+
 }
