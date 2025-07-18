20e2379ee3e010fba8d98fbcd718dbf906d7541e
Wanderson Brito Santos <wbsmsn@hotmail.com>
2025-07-18 14:08:35 -0300

    ajuste para mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 document.addEventListener('DOMContentLoaded',function(){
    const buttons = document.querySelectorAll('[data-tab-button]'); //aponta para todos os bottoes 
-    
+   const questions = document.querySelectorAll('[data-faq-question]')
+  
+   const heroSection = document.querySelector('.hero');
+   console.log(heroSection)
+   const alturaHero = heroSection.clientHeight;
 
+   window.addEventListener('scroll', function(){
+      const posicaoAtual = window.scrollY;
+
+      if(posicaoAtual < alturaHero){
+
+             console.log("ocultar")
+
+             ocultarElementosDoHeader();
+      }
+      else{
+        
+        exibeElementosDoHeader();
+      }
+   })
+
+   //para secao de atraçoes , programaçao das abas 
    for(let i=0; i < buttons.length; i++){
        buttons[i].addEventListener('click', function(botao){
           const abaAlvo = botao.target.dataset.tabButton;
@@ -12,6 +32,28 @@ document.addEventListener('DOMContentLoaded',function(){
            botao.target.classList.add('shows__tabs__button--is-active');
        })
    }
+  //secao pra acordion 
+   for(let i = 0; i < questions.length;i++){
+        questions[i].addEventListener('click',abreOuFechaResposta);
+   }
+ 
+   function ocultarElementosDoHeader(){
+         const header = document.querySelector('header');
+         header.classList.add('header--is-hidden');
+   }
+   
+    function exibeElementosDoHeader(){
+         const header = document.querySelector('header');
+         header.classList.remove('header--is-hidden');
+   }
+
+
+   function abreOuFechaResposta(el){
+       const classe ='faq__questions__item--is-open';
+       const elPai = el.target.parentNode;
+       elPai.classList.toggle(classe)
+   } 
+
 })
 
 function removeBotaoAtivo(){
